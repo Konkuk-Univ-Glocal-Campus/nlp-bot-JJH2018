10c9c1e0e191848eef225b5db48f968e891198e8
JJH2018 <164473110+JJH2018@users.noreply.github.com>
2024-04-29 12:24:40 +0000

    아웃풋으로 보이는 모든 영어를 한국어로 번역하였습니다.  또한 인풋으로 넣어야하는 'bye'를 '잘가'로 바꾸었습니다.

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
 import random
 
 # This list contains the random responses (you can add your own or translate them into your own language too)
-random_responses = ["That is quite interesting, please tell me more.",
-                    "I see. Do go on.",
-                    "Why do you say that?",
-                    "Funny weather we've been having, isn't it?",
-                    "Let's change the subject.",
-                    "Did you catch the game last night?"]
+random_responses = ["흥미로운 이야기네요. 더 이야기해주세요",
+                    "알겠어요. 더 말씀해주시죠",
+                    "왜 그렇게 말씀하시죠?",
+                    "요새 날씨가 이상하지 않나요?",
+                    "화제를 바꾸죠.",
+                    "어제 밤에 있던 경기 보셨나요?"]
 
-print("Hello, I am Marvin, the simple robot.")
-print("You can end this conversation at any time by typing 'bye'")
-print("After typing each answer, press 'enter'")
-print("How are you today?")
+print("안녕하세요. 저는 단순 챗봇 마빈이라고 합니다.")
+print("당신이 이 대화를 끝내고 싶다면 '잘가'라고 말씀해주세요.")
+print("답변을 입력한 후 '엔터'를 누르세요.")
+print("오늘은 어땠습니까?")
 
 while True:
     # wait for the user to enter some text
     user_input = input("> ")
-    if user_input.lower() == "bye":
+    if user_input.lower() == "잘가":
         # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
         break
     else:
         response = random.choices(random_responses)[0]
     print(response)
 
-print("It was nice talking to you, goodbye!")
+print("좋은 대화였어요! 다음에 봐요!")
